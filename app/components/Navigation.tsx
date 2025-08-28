65a99583a5ee8258ac294ca75da025fa0ac28279
Marla Tumenjargal <tumearla@gmail.com>
2025-08-27 23:01:44 -0700

    updated resume

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -8,7 +8,7 @@ const navItems = [
 ];
 
 const externalLinks = [
-  { name: "resume", href: "https://drive.google.com/file/d/1v482f4wz601Ozl_7To21phPzfwHg6342/view?usp=sharing" },
+  { name: "resume", href: "https://drive.google.com/file/d/19O75d9YxXDIqCBSt4WZI0psZS-XVwsU6/view?usp=sharing" },
   { name: "linkedin", href: "https://www.linkedin.com/in/marla-tumenjargal/" },
   { name: "github", href: "https://github.com/marla-tumenjargal" },
 ];
