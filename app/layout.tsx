2574734e2b040428e2e3e4ae2478d8c4abeeaffc
Marla Tumenjargal <tumearla@gmail.com>
2025-09-07 23:22:24 -0700

    analysticS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 import Header from './components/Navigation';
 import Footer from './components/Footer';
