4cd2f82143493bc869b05fd546ca0103517f6bd4
Marla Tumenjargal <tumearla@gmail.com>
2025-09-07 23:15:47 -0700

    updated writing

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -25,31 +25,37 @@ export default function WritingPage() {
       images: ['/blueruf.jpg'],
       content: `
       
-    when Darwin created his Theory of Evolution, some may say that the "time was ripe." if Darwin never lived, some other scientist would have 
-    coined it in his place. but let's look at another example: Gregor Mendel! when he discovered the punnett square, his work was barely 
-    acknowledged. this, in part, is because he was quite ahead of his time. he combined two completely disparaging fields (math and biology) 
-    together to radically innovate (we call this radical innovation).
-      
-    this fall semester, i'm taking Data 94, which explores the social dynamics behind computational thinking, including topics such as AGI, 
-    recommendation systems, the Turing test as a marker of intelligence, and free speech online. behind the fundamental questions of
-    technology's role in society is binary. binary is credited to Leibniz. similarly to Mendel, he combined two different fields 
-    (logic and algebra) together to create a "language" of sorts--that is the precursor to probabilistic inference.     
-    he proved that everything done by our mind is possible with computation. he believed math was a language. private language can not exist.
-    you can not express a concept if it does not have a name (again, why we don't have memories as babies because we can not conceptualize 
-    what we do not yet know) and math is a language and language is simply made up of symbols/signs thus, we can not think or reason or 
-    compute to ourselves without language (in this case, language is math + logic). binary is only concerned with 0 and 1s, on or off. 
-    
-    one fundamental logic behind this idea are the three laws of thought. these three laws are the law of identity, law of contradiction, and 
-    the law of excluded middle. leibniz believed all knowledge is self evident, meaning that the mind itself already knows. for example, 
-    a triangle is self evidence because you can not comprehend of a triangle without three sides. another example is a bachelor. a bachelor 
-    is always unmarried. other knowledge (he calls it empirical knowledge) is found "through" other knowledge. for example, if i say "kidney" 
-    you would not intuitively correlate that with "pumping blood." most empirical knowledge drives scientific knowledge since we always
-     discovering new facts that continue creating new knowledge. we do not think through these three laws of thought but we experience and 
-     behave through them. 
-      
-    leibniz also thought that there must be a metaphysical grounding to everything. binary is either on or off. 
-    it has a certain specialness to it because that is how humans think. we can not know what is good but we do understand 
-    good" as the opposite of evil. these are quite messy thoughts, but i think they are quite interesting.`
+when Darwin created his Theory of Evolution, some may say that the "time was ripe." if Darwin never
+coined it in his place. but let's look at another example: Gregor Mendel! when he discovered the
+punnett square, his work was barely acknowledged. this, in part, is because he was quite ahead of his
+time. he combined two completely disparaging fields (math and biology) together to radically innovate
+(we call this radical innovation).
+
+this fall semester, i'm taking Data 94, which explores the social dynamics behind computational
+thinking, including topics such as AGI, recommendation systems, the Turing test as a marker of
+intelligence, and free speech online. behind the fundamental questions of technology's role in society
+is binary. binary is credited to Leibniz. similarly to Mendel, he combined two different fields (logic
+and algebra) together to create a "language" of sorts--that is the precursor to probabilistic inference.
+he proved that everything done by our mind is possible with computation. he believed math was a language.
+private language can not exist. you can not express a concept if it does not have a name (again, why we
+don't have memories as babies because we can not conceptualize what we do not yet know) and math is a
+language and language is simply made up of symbols/signs thus, we can not think or reason or compute
+to ourselves without language (in this case, language is math + logic). binary is only concerned with 0
+and 1s, on or off.
+
+one fundamental logic behind this idea are the three laws of thought. these three laws are the law of
+identity, law of contradiction, and the law of excluded middle. <span class="highlight">leibniz believed all knowledge is self
+evident, meaning that the mind itself already knows.</span class="highlight"> for example, a triangle is self evidence because you
+can not comprehend of a triangle without three sides. another example is a bachelor. a bachelor is always
+unmarried. other knowledge (he calls it empirical knowledge) is found "through" other knowledge. for
+example, if i say "kidney" you would not intuitively correlate that with "pumping blood." most empirical
+knowledge drives scientific knowledge since we always discovering new facts that continue creating new
+knowledge. <span class="highlight">we do not think through these three laws of thought but we experience and behave through them.</span class="highlight">
+
+leibniz also thought that there must be a metaphysical grounding to everything. binary is either on or off.
+it has a certain specialness to it because that is how humans think. we can not know what is good but we
+do understand good" as the opposite of evil. <span class="highlight">these are quite messy thoughts, but i think they are quite
+interesting.</span class="highlight">`
     },
     {
       id: 2,
